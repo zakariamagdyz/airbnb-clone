2ba4cb25024524a7ee2cec2500a53e88ff407da1
zakariamagdyz <zakariamagdy@ymail.com>
2023-08-22 23:59:03 +0300

    feat: integrated husky, commitlint, lint-staged for enhanced git hooks and commit message standards

---
+++ .lintstagedrc.js
@@ -0,0 +1,11 @@
+const path = require("path")
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => (f !== ".lintstagedrc.js" ? path.relative(process.cwd(), f) : ""))
+    .join(" --file ")}`
+
+module.exports = {
+  "src/**/*.{js,jsx,ts,tsx,css,md,json}": "prettier -uw --cache",
+  "src/**/*.{js,jsx,ts,tsx}": [buildEslintCommand],
+}
