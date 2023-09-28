8e690f72a73c6947c32d3532a15928de9dc9c2da
zakariamagdyz <zakariamagdy@ymail.com>
2023-09-28 14:56:26 +0300

    feat: update eslint-plugin-testing-lib rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -48,14 +48,6 @@ module.exports = {
       ],
       plugins: ['jest', 'jest-dom', 'testing-library'],
       rules: {
-        'jest-dom/prefer-checked': 'error',
-        'jest-dom/prefer-enabled-disabled': 'error',
-        'jest-dom/prefer-required': 'error',
-        'jest-dom/prefer-to-have-attribute': 'error',
-        'testing-library/await-async-query': 'error',
-        'testing-library/no-await-sync-query': 'error',
-        'testing-library/no-debugging-utils': 'warn',
-        'testing-library/no-dom-import': 'off',
         'testing-library/no-unnecessary-act': 'off', // Turn off cause RHF need click actions wrapped in act
       },
     },
